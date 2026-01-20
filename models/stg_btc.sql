e9d9ae0475dde11dd5690acb080a69a8de4391cf
Tomek Robert <asterixobelix1984@gmail.com>
2026-01-20 11:29:37 +0000

    l

---
+++ models/stg_btc.sql
@@ -0,0 +1,28 @@
+use btc
+
+
+
+{{config(materialized='incremental',incremental_strategy='merge', unique_key='HASH_KEY')}}
+
+
+SELECT 
+* 
+FROM
+{{ source('btc', 'btc') }}
+
+--Links used in this lecture:
+
+
+-- https://docs.getdbt.com/docs/build/incremental-models
+
+-- https://docs.getdbt.com/docs/build/incremental-strategy
+
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+where BLOCK_TIMESTAMP >= (select max(BLOCK_TIMESTAMP) from {{ this }} )
+
+{% endif %}
